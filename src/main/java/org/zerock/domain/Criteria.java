1a15853a83443a28fd4dd3967d1176bb2d7c8c1c
id91y <id91y@naver.com>
2021-06-25 19:11:02 +0900

    a

--- src/main/java/org/zerock/domain/Criteria.java
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -8,26 +8,45 @@ import lombok.ToString;
 @Setter
 @ToString
 public class Criteria {
-
 	
-		private int pageNum;
-		private int amount;
-		
-		public Criteria() {
-			this(1,10);
+	private int pageNum;
+	private int amount;
+	
+	private String type;
+	private String keyword;
+	
+	public Criteria() {
+		this(1, 10);
+	}
+	
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+	
+	public int getFrom() {
+		return amount * (pageNum - 1);
+	}
+	
+	public String[] getTypeArr() {
+		if (type == null) {
+			return new String[] {};
+		} else {
+			String[] types = type.split("");
+			return types;
 		}
-		
-		
-		public Criteria(int pageNum, int amount) {
-			this.pageNum = pageNum;
-			this.amount = amount;
-		
-		}
-		
-		public int getFrom() {
-			return amount * (pageNum -1);
-			
-		}
-		
-		
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
