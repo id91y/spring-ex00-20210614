964defd5f460d07032b79608b5cfef8e05f08917
id91y <id91y@naver.com>
2021-06-29 16:39:59 +0900

    a

---
+++ src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -0,0 +1,64 @@
+package org.zerock.mapper;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.ReplyVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class ReplyMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private ReplyMapper mapper;
+	
+	@Test
+	public void testMapper() {
+		assertNotNull(mapper);
+	}
+	
+	@Test
+	public void testInsert() {
+		ReplyVO vo = new ReplyVO();
+		vo.setReply("댓글!!!");
+		vo.setReplyer("user00");
+		vo.setBno(38L);
+		
+		// Long 
+		// long
+		
+		int cnt = mapper.insert(vo);
+		assertEquals(1, cnt);
+	}
+	
+	
+	@Test
+	public void testRead() {
+		ReplyVO vo = mapper.read(1L);
+		
+		assertEquals("댓글!!!", vo.getReply());
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
